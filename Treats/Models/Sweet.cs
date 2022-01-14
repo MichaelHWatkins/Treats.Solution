1d4223079f82ce4e413a887077bd06aa319dba2b
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2022-01-14 09:01:29 -0800

    add the Sweet model

---
+++ Treats/Models/Sweet.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Treats.Models
+{
+public class Sweet
+    {
+        public Sweet()
+        {
+            this.JoinEntities = new HashSet<SweetFlavor>();
+        }
+
+        public int SweetId { get; set; }
+        public string SweetName { get; set; }
+        public virtual ICollection<SweetFlavor> JoinEntities { get; set; }
+    }
+}
