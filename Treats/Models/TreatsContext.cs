cb87ccc17cc3a93f28a97683d5098b8553612c28
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2022-01-14 08:52:03 -0800

    add the treatscontext

---
+++ Treats/Models/TreatsContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Treats.Models
+{
+  public class TreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Sweet> Sweets { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<SweetFlavor> SweetFlavor { get; set; }
+
+    public TreatsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
