c45d46c9ff36215c1a50cda4a598b9f2805074cc
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2022-01-14 11:19:13 -0800

    add the Flavors Controller

--- Treats/Controllers/FlavorsController.cs
+++ Treats/Controllers/FlavorsController.cs
@@ -14,11 +14,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace Treats.Controllers
 {
   [Authorize]
-  public class SweetsController : Controller
+  public class FlavorsController : Controller
   {
     private readonly TreatsContext _db;
     private readonly UserManager<ApplicationUser> _userManager;
-    public SweetsController(UserManager<ApplicationUser> userManager, TreatsContext db)
+    public FlavorsController(UserManager<ApplicationUser> userManager, TreatsContext db)
     {
       _userManager = userManager;
       _db = db;
@@ -28,27 +28,27 @@ namespace Treats.Controllers
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
-      var userSweets = _db.Sweets.Where(entry => entry.User.Id == currentUser.Id).ToList();
-      return View(userSweets);
+      var userFlavors = _db.Flavors.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userFlavors);
     }
     
     public ActionResult Create()
     {
-        ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+        ViewBag.SweetId = new SelectList(_db.Sweets, "SweetId", "SweetName");
         return View();
     }
 
     [HttpPost]
-    public async Task<ActionResult> Create(Sweet sweet, int FlavorId)
+    public async Task<ActionResult> Create(Flavor Flavor, int SweetId)
     {
         var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         var currentUser = await _userManager.FindByIdAsync(userId);
-        sweet.User = currentUser;
-        _db.Sweets.Add(sweet);
+        flavor.User = currentUser;
+        _db.Flavors.Add(flavor);
         _db.SaveChanges();
-        if (FlavorId != 0)
+        if (SweetId != 0)
         {
-            _db.SweetFlavor.Add(new SweetFlavor() { FlavorId = FlavorId, SweetId = sweet.SweetId });
+            _db.SweetFlavor.Add(new SweetFlavor() { SweetId = SweetId, FlavorId = flavor.FlavorId });
         }
         _db.SaveChanges();
         return RedirectToAction("Index");
@@ -56,42 +56,42 @@ namespace Treats.Controllers
 
     public ActionResult Details(int id)
     {
-        var thisSweet = _db.Sweets
-        .Include(sweet => sweet.JoinEntities)
-        .ThenInclude(join => join.Flavor)
-        .FirstOrDefault(sweet => sweet.SweetId == id);
-        return View(thisSweet);
+        var thisFlavor = _db.Flavors
+        .Include(flavor => flavor.JoinEntities)
+        .ThenInclude(join => join.Sweet)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+        return View(thisFlavor);
     }
 
     public ActionResult Edit(int id)
     {
-        var thisSweet = _db.Sweets.FirstOrDefault(sweet => sweet.SweetId == id);
-        ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
-        return View(thisSweet);
+        var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+        ViewBag.SweetId = new SelectList(_db.Sweets, "SweetId", "SweetName");
+        return View(thisFlavor);
     }
 
     [HttpPost]
-    public ActionResult Edit(Sweet sweet, int FlavorId)
+    public ActionResult Edit(Flavor flavor, int SweetId)
     {
-      if (FlavorId != 0)
+      if (SweetId != 0)
       {
-        _db.SweetFlavor.Add(new SweetFlavor() { FlavorId = FlavorId, SweetId = sweet.SweetId });
+        _db.SweetFlavor.Add(new SweetFlavor() { SweetId = SweetId, FlavorId = flavor.FlavorId });
       }
-      _db.Entry(sweet).State = EntityState.Modified;
+      _db.Entry(flavor).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
     public ActionResult Delete(int id)
     {
-        var thisSweet = _db.Sweets.FirstOrDefault(sweet => sweet.SweetId == id);
-        return View(thisSweet);
+        var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+        return View(thisFlavor);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-        var thisSweet = _db.Sweets.FirstOrDefault(sweet => sweet.SweetId == id);
-        _db.Items.Remove(thisSweet);
+        var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+        _db.Items.Remove(thisFlavor);
         _db.SaveChanges();
         return RedirectToAction("Index");
     }
